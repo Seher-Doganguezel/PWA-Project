7a5987cc3611732b6829a114c9ceb11df9cc9d30
Seher Dogangüzel <seker.seher@hotmail.de>
2023-07-02 23:32:53 +0200

    server.worker node start

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,16 @@ importScripts(
       ({request}) => request.destination === 'image',
       new workbox.strategies.NetworkFirst()     // NetworkFirst() vs CacheFirst()
   )
+
+  self.addEventListener('install', event => {
+    console.log('service worker --> installing ...', event);
+})
+
+self.addEventListener('activate', event => {
+    console.log('service worker --> activating ...', event);
+    return self.clients.claim();
+})
+
+self.addEventListener('fetch', event => {
+    console.log('service worker --> fetching ...', event);
+})
